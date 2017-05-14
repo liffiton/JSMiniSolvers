2bca9753c21decbe01107f67f075a0be0c243425
Mark Liffiton <liffiton@gmail.com>
2017-05-13 20:13:20 -0500

    Simplification (libminisat -> lib)

--- src/minisolvers.js
+++ src/minisolvers.js
@@ -1,42 +1,42 @@
-import libminisat from './cpp/libminisat.js';
+import lib from './cpp/libminisat.js';
 
 class MinisatSolver {
     constructor() {
-        this.S = libminisat.ccall('Solver_new', 'number', []);
+        this.S = lib.ccall('Solver_new', 'number', []);
     }
 
     delete() {
-        libminisat.ccall('Solver_delete', 'number', ['number'], [this.S]);
+        lib.ccall('Solver_delete', 'number', ['number'], [this.S]);
     }
 
     nvars() {
-        return libminisat.ccall('nVars', 'number', ['number'], [this.S]);
+        return lib.ccall('nVars', 'number', ['number'], [this.S]);
     }
 
     nclauses() {
-        return libminisat.ccall('nClauses', 'number', ['number'], [this.S]);
+        return lib.ccall('nClauses', 'number', ['number'], [this.S]);
     }
 
     new_var() {
-        return libminisat.ccall('newVar', 'number', ['number', 'number', 'number'], [this.S, 2, 1]);
+        return lib.ccall('newVar', 'number', ['number', 'number', 'number'], [this.S, 2, 1]);
     }
 
     add_clause(clause) {
         // TODO: checking, singleton detection (see pyminisolvers)
-        return libminisat.ccall('addClause', 'number', ['number', 'number', 'array'], [this.S, clause.length, new Uint8Array(new Int32Array(clause).buffer)]);
+        return lib.ccall('addClause', 'number', ['number', 'number', 'array'], [this.S, clause.length, new Uint8Array(new Int32Array(clause).buffer)]);
     }
 
     solve() {
-        return libminisat.ccall('solve', 'number', ['number'], [this.S]);
+        return lib.ccall('solve', 'number', ['number'], [this.S]);
     }
 
     get_model() {
         var nv = this.nvars();
-        var ptr = libminisat._malloc(4 * nv);
-        var modelarr = libminisat.HEAP32.subarray((ptr>>2), (ptr>>2)+nv);
-        libminisat.ccall('fillModel', null, ['number', 'number', 'number', 'number'], [this.S, ptr, 0, this.nvars()]);
+        var ptr = lib._malloc(4 * nv);
+        var modelarr = lib.HEAP32.subarray((ptr>>2), (ptr>>2)+nv);
+        lib.ccall('fillModel', null, ['number', 'number', 'number', 'number'], [this.S, ptr, 0, this.nvars()]);
         var model = Array.from(modelarr);
-        libminisat._free(ptr);
+        lib._free(ptr);
         return model;
     }
 }
