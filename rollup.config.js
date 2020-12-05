a7a82a126d8e7944ae176c25cd4072fc7b650284
Mark Liffiton <liffiton@gmail.com>
2020-12-04 22:49:53 -0600

    Update node packages.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 
 import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { uglify } from "rollup-plugin-uglify";
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 export default {
     input: 'src/index.js',
@@ -20,8 +20,9 @@ export default {
             ignore: [ 'fs', 'path' ]
         }),
         babel({
-            exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled'
         }),
-        uglify(),
+        terser(),
     ]
 };
