d877a172d9d30d86078c7111cb53b77ff41ed257
Mark Liffiton <liffiton@gmail.com>
2022-07-10 15:31:39 -0500

    Bump versions, including a major version bump for Ava.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 
